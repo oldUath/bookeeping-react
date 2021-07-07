cf0caf595fea7c5e9e15f2cdcfd963f3e2f5c1c1
oldUath <haiyangdong666@gmail.com>
2021-07-07 22:37:13 +0800

    路由&导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,84 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from 'react-router-dom';
+import styled from "styled-components";
+import './index.scss'
+
+const Wrapper = styled.div`
+  border: 1px solid red;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.div`
+  border: 1px solid green;
+  flex-grow: 1;
+  overflow: auto;
+`
+const Nav = styled.nav`
+  >ul{
+    display: flex;
+    >li{
+      width: 33.3333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`
+
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+        <Wrapper>
+            <Main>
+              <Switch>
+                <Route path="/tags">
+                  <Tags />
+                </Route>
+                <Route path="/money">
+                  <Money />
+                </Route>
+                <Route path="/statistics">
+                  <Statistics />
+                </Route>
+                <Redirect exact from="/" to="/money" />
+                <Route path="*">
+                  <NoMatch />
+                </Route>
+              </Switch>
+            </Main>
+
+            <Nav>
+                <ul>
+                    <li><Link to="/tags">Home</Link></li>
+                    <li><Link to="/money">About</Link></li>
+                    <li><Link to="/statistics">User</Link></li>
+                </ul>
+            </Nav>
+        </Wrapper>
+    </Router>
   );
 }
 
-export default App;
+function Statistics(){
+  return<h2>统计页面</h2>
+}
+function Tags(){
+  return <h2>标签页面</h2>
+}
+function Money(){
+  return <h2>记账叶</h2>
+}
+function NoMatch(){
+  return <h2>NoMatch</h2>
+}
+
+
+
+export default App
