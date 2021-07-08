5882c98ebd3ab754f6f288d2c939f6d5f2e27bfa
oldUath <haiyangdong666@gmail.com>
2021-07-08 21:41:51 +0800

    封装Layout组件

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,17 @@ import {
   Switch,
   Redirect,
 } from 'react-router-dom';
-import styled from "styled-components";
 import './index.scss'
-import Nav from './components/Nav'
+import Layout from "./components/Layout";
 
 
-const Wrapper = styled.div`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-`
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`
+
 
 
 
 function App() {
   return (
     <Router>
-        <Wrapper>
-            <Main>
               <Switch>
                 <Route path="/tags">
                   <Tags />
@@ -42,21 +31,30 @@ function App() {
                   <NoMatch />
                 </Route>
               </Switch>
-            </Main>
-            <Nav />
-        </Wrapper>
     </Router>
   );
 }
 
 function Statistics(){
-  return<h2>统计页面</h2>
+  return (
+    <Layout>
+        <h2>统计页面</h2>
+    </Layout>
+  )
 }
 function Tags(){
-  return <h2>标签页面</h2>
+  return(
+    <Layout>
+        <h2>标签页面</h2>
+    </Layout>
+  )
 }
 function Money(){
-  return <h2>记账叶</h2>
+  return(
+    <Layout>
+        <h2>记账</h2>
+    </Layout>
+  )
 }
 function NoMatch(){
   return <h2>NoMatch</h2>
